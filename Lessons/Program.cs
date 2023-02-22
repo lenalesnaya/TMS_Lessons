f0f85c2adf2a614306608ec23808b8e8f41d5090
lenalesnaya <lenalesnaya1993@gmail.com>
2023-02-22 21:30:52 +0300

    The task of the first lesson is moved to a separate method.

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -4,9 +4,17 @@
     {
         static void Main(string[] args)
         {
+            L1T1_HelloName();
+        }
+
+
+        #region Lesson_1
+        public static void L1T1_HelloName()
+        {
             Console.WriteLine("Введите имя существа, которому хотите передать привет:");
             var name = Console.ReadLine();
             Console.WriteLine("Привет, " + name);
         }
+        #endregion
     }
 }
